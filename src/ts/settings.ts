47315a85c8a32fba97364ebb0dfcd35449bada3d
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-02-13 14:15:38 +0100

    :tada: Initial commit

---
+++ src/ts/settings.ts
@@ -0,0 +1,15 @@
+export const settings = {
+    swissFlag: {
+        rect: {
+            width: 280,
+            height: 65,
+            backgroundColor: "white",
+        },
+        rect2: {
+            width: 65,
+            height: 280,
+            backgroundColor: "white",
+        },
+        backgroundColor: "red"
+    }
+}
